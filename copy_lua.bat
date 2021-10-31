6b3c95609ae10187581cc08ec11dc28cfba42dff
Lukáš Baštýř <l.bastyr@seznam.cz>
2021-10-31 20:37:06 +0100

    Auto stash before merge of "master" and "origin/master"

--- copy_lua.bat
+++ copy_lua.bat
@@ -1,1 +1,1 @@
-xcopy "O:\This PC\Documents\0_Projects\ISA\dissector.lua" "C:\Users\lbast\AppData\Roaming\Wireshark\plugins\3.4\" /y
+xcopy "C:\Users\lbast\Documents\0_Projects\ISA\dissector.lua" "C:\Users\lbast\AppData\Roaming\Wireshark\plugins\3.4\" /y
